a9f8ed747f1459aa49f2fd4bb84fde8aac839a3c
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-05-27 16:48:13 +0300

    taxi-service was implemented

---
+++ src/main/java/taxi/model/Driver.java
@@ -0,0 +1,82 @@
+package taxi.model;
+
+import java.util.Objects;
+
+public class Driver {
+    private Long id;
+    private String login;
+    private String password;
+    private String name;
+    private String licenseNumber;
+
+    public Driver() {
+    }
+
+    public Driver(String login, String password, String name, String licenseNumber) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Driver driver = (Driver) o;
+        return id.equals(driver.id)
+                && name.equals(driver.name)
+                && licenseNumber.equals(driver.licenseNumber)
+                && login.equals(driver.login)
+                && password.equals(driver.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, licenseNumber, login, password);
+    }
+}
